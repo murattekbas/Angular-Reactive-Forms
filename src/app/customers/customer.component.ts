d1247b94205117f3c95b6f8398c02a9ca53790c4
murattekbas <muratekbas@gmail.com>
2022-01-03 16:44:10 +0300

    Building a Reactive Form

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -12,21 +12,41 @@ export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
     //Form Model
+    this.customerForm=this.fb.group({
+      firstName:'',
+      // lastName:{value:'n/a',disabled:true},
+      lastName:'',
+      email:'',
+      sendCatalog:true,
+    })
+
+
+/* 
     this.customerForm=new FormGroup({
       firstName:new FormControl(),
       lastName:new FormControl(),
       email:new FormControl(),
       sendCatalog:new FormControl(true),
 
-    });
+    }); */
   }
 
   save(){
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData(){
+    //use patchValue for setting partial members
+    this.customerForm.setValue({
+      firstName:'Jack',
+      lastName:'Harkness',
+      email:'jack@torchwood.com',
+      sendCatalog:false
+    });
   }
 }
